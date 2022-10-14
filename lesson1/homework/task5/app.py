ac259dfed377c8246d6698ba1fe713f27f8c979d
abilardo <don.abilardo@gmail.com>
2022-10-14 14:56:12 +0500

    Расстояние между 2 точками на отрезке

--- lesson1/homework/task5/app.py
+++ lesson1/homework/task5/app.py
@@ -1,2 +1,25 @@
 """ 5. Напишите программу, которая принимает на вход координаты двух точек
- и находит расстояние между ними в 2D пространстве. " ""
+и находит расстояние между ними в 2D пространстве.   """
+
+#from unittest import result
+
+
+pointA = float(input("Координа точки А >>> "))
+pointB = float(input("Координа точки B >>> "))
+
+
+def dist (pointA, pointB):
+    if pointA < 0:
+      pointA = pointA * -1
+    elif pointB < 0:
+        pointB = pointB * -1
+
+    if pointA > pointB:
+        result = pointA - pointB
+    elif pointB > pointA:
+        result = pointB - pointA
+    elif pointA == pointB:
+        result = 0
+    return result
+
+print(dist(pointA, pointB))
