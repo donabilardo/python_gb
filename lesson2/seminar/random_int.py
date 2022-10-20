94e5eecf4e7b5452bb4a839de8f2acd535e46275
abilardo <don.abilardo@gmail.com>
2022-10-20 23:10:04 +0500

    генерация случайных чисел через цикл while

--- lesson2/seminar/random_int.py
+++ lesson2/seminar/random_int.py
@@ -6,12 +6,21 @@ startInt = int(input("Начало диапазона :  "))
 endInt = int(input("Конец диапазона :  "))
 
 
-def randDigit(startInt, endInt, lenInt):
+def randDigitFor(startInt, endInt, lenInt):
     listRandom = []
     for i in range(lenInt):
         listRandom.append(randint(startInt, endInt))
     return listRandom
 
 
-aList = randDigit(startInt, endInt, lenInt)
+def randDigitWhile(startInt, endInt, lenInt):
+    listRandom = []
+    i = 0
+    while i < lenInt:
+        listRandom.append(randint(startInt, endInt))
+        i = i+1
+    return listRandom
+
+
+aList = randDigitWhile(startInt, endInt, lenInt)
 print(aList)
