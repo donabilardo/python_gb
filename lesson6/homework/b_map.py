87965a8af33eae06c58110a7f06366f8ba5ed4b1
abilardo <don.abilardo@gmail.com>
2022-11-10 17:28:34 +0500

    Добавил функцию

--- lesson6/homework/b_map.py
+++ lesson6/homework/b_map.py
@@ -6,10 +6,10 @@ import a_list_сomprehension
 
 mySuperList = a_list_сomprehension.getRandomList()
 
-myListStr = list(map(str,mySuperList)) #Применяем функцию str (которая приводит к строке) ко всем элементам списка, полученым с помощью list comprehension
+def convertListToString(lst):
+    lst = list(map(str,mySuperList))#Применяем функцию str (которая приводит к строке) ко всем элементам списка, полученым с помощью list comprehension
+    return lst
 
 
-print(myListStr) #получили строку
-
-
+myList = convertListToString(mySuperList)
 
