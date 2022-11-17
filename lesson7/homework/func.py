528e5e85979d5fc8b924c5ad2c0e8118418635cf
abilardo <don.abilardo@gmail.com>
2022-11-17 17:17:09 +0500

    модуль с функциями

---
+++ lesson7/homework/func.py
@@ -0,0 +1,11 @@
+import re
+
+def formatStr(data):
+    result = "".join(c for c in data if c.isalpha() and " ")
+    return result
+
+def joinStr(a,b,c)
+
+def formatInt(data):
+    result = "".join(c for c in data if  c.isdecimal())
+    return result
