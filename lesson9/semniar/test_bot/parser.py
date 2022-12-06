65e3df5f0d6e47bc2a5152e28c2b525e1a648aa6
Don Abilardo <don.abilardo@gmail.com>
2022-12-06 06:50:44 +0500

    Добавлены библиотеки

--- lesson9/semniar/test_bot/parser.py
+++ lesson9/semniar/test_bot/parser.py
@@ -1,14 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import lxml
+import csv
 
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36"
+header = {
+    "accept" : "*/*",
+    "sec-ch-ua-platform" : "Windows",
+    "sec-ch-ua" : "'Not A;Brand';v='99', 'Chromium';v='101', 'Google Chrome';v='101'",
+    "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36"
+}
 URL = "https://kinoprostor.ru/"
 
-with open("prostor.html", "r", encoding="utf-8") as html_file:
+req = requests.get(URL, headers=header)
+
+src_html = req.text
+
+""" with open("prostor.html", "r", encoding="utf-8") as html_file:
     src = html_file.read()
 
-soup = bs(src, "lxml")
+soup = bs(src, "lxml") """
 
 
-print(soup)
+print(src_html)
